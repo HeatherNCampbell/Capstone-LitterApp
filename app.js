9de7b52b808b73906c3878709b906f2d1ebe5eeb
hcampbell1212 <heather.campbell1212@gmail.com>
2021-05-06 13:47:42 -0500

    Style Update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const address = document.getElementById("address");
 
 const mapImage = document.getElementById("mapImage");
 
-mapImage.addEventListener('click',() => {address.innerText = "Trojan Trail, Glasgow, KY 42141"});
+mapImage.addEventListener('click',() => {address.innerText = "BC Skills, Trojan Trail, Glasgow, KY 42141"});
 
 
 
