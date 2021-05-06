0e2fcfbda45dcc808a1320d843740923e1060b9a
hcampbell1212 <heather.campbell1212@gmail.com>
2021-05-05 22:46:00 -0500

    LitterApp html, css, js. Need to designate required fields, create thumbpring after image upload, verify submit works, additional styling

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+const address = document.getElementById("address");
+
+const mapImage = document.getElementById("mapImage");
+
+mapImage.addEventListener('click',() => {address.innerText = "Trojan Trail, Glasgow, KY 42141"});
+
+
+
+
